b018d6919d946d2fb9217a6247b9725935214d2b
waamir104 <waamirdev@gmail.com>
2023-11-05 13:29:56 -0500

    Initializing Hotel Alura Api project

---
+++ src/main/java/dev/waamir/hotelaluraapi/HotelAluraApiApplication.java
@@ -0,0 +1,13 @@
+package dev.waamir.hotelaluraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAluraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/waamir/hotelaluraapi/HotelAluraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.waamir.hotelaluraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAluraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
