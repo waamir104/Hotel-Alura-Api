224c2c1f1a9b58ef1b49534182560bff68e2e545
waamir104 <waamirdev@gmail.com>
2023-11-09 16:28:02 -0400

    Fixing the getByUser and deleteById methods

--- src/main/java/dev/waamir/hotelaluraapi/domain/port/IAccountVerificationRepository.java
+++ src/main/java/dev/waamir/hotelaluraapi/domain/port/IAccountVerificationRepository.java
@@ -1,12 +1,13 @@
 package dev.waamir.hotelaluraapi.domain.port;
 
 import dev.waamir.hotelaluraapi.domain.model.AccountVerification;
+import dev.waamir.hotelaluraapi.domain.model.User;
 
 import java.util.Optional;
 
 public interface IAccountVerificationRepository<T extends AccountVerification> {
     T create(T t);
-    Optional<T> getByUser(T t);
-    void deleteById();
+    Optional<T> getByUser(User user);
+    void deleteById(Long id);
     void update(T t);
 }
