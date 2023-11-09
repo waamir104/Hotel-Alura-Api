2678aad8524aba1ea9060220d37f145a34334d9b
waamir104 <waamirdev@gmail.com>
2023-11-09 17:30:56 -0400

    Changing the parameter data type of the method getByNumber in the IRoomRepository from the domain/port directory

--- src/main/java/dev/waamir/hotelaluraapi/domain/port/IRoomRepository.java
+++ src/main/java/dev/waamir/hotelaluraapi/domain/port/IRoomRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface IRoomRepository<T extends Room> {
     T create(T t);
     Optional<T> getById(Long id);
-    Optional<T> getByNumber(int number);
+    Optional<T> getByNumber(Long number);
     void delete(T t);
     void update(T t);
     List<T> list();
