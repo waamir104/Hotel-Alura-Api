954de080452198a58cf4436822e799bb021bb736
waamir104 <waamirdev@gmail.com>
2023-11-13 14:20:28 -0400

    Adding the beans for de Base64 Encoder and Decoder in the ApplicationConfig file from the spring/config directory

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/config/ApplicationConfig.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/config/ApplicationConfig.java
@@ -1,5 +1,9 @@
 package dev.waamir.hotelaluraapi.infrastructure.rest.spring.config;
 
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,5 +15,15 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Encoder base64Encoder() {
+        return Base64.getEncoder();
+    }
+
+    @Bean 
+    public Decoder base64Decoder() {
+        return Base64.getDecoder();
     }
 }
