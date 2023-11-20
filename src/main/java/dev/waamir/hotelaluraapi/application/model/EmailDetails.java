eb9327cbac462a7922ce49b0e9d0a93ef1134ee4
waamir104 <waamirdev@gmail.com>
2023-11-20 17:04:22 -0400

    Adding class to transfer data of an email

---
+++ src/main/java/dev/waamir/hotelaluraapi/application/model/EmailDetails.java
@@ -0,0 +1,14 @@
+package dev.waamir.hotelaluraapi.application.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+}
