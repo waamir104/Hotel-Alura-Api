d0c784338bd9a5e548b2b0ac2ecf2da032302f48
waamir104 <waamirdev@gmail.com>
2023-11-21 19:25:53 -0400

    Adding a CLR for testing the roleRepository and a Resource for a demo

---
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/DemoResource.java
@@ -0,0 +1,16 @@
+package dev.waamir.hotelaluraapi.infrastructure.rest.spring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoResource {
+    
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello World, it works!!");
+    }
+}
