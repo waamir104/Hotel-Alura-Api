0eca136a497404baa50522c5984e2fddc37fadb2
waamir104 <waamirdev@gmail.com>
2023-11-26 18:31:22 -0400

    Adding the BadCredentialsException handler to the ExceptionHandlerAdvice file

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/exception/ExceptionHandlerAdvice.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/exception/ExceptionHandlerAdvice.java
@@ -1,7 +1,9 @@
 package dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +41,17 @@ public class ExceptionHandlerAdvice {
             .body(
                 MessageResponse.builder()
                     .message("User is disabled. Please verify the account or contact the admin.")
+                    .build()
+            );
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<MessageResponse> handleBadCreditials() {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                MessageResponse.builder()
+                    .message("Invalid login credentials. Please verify the data and try again.")
                     .build()
             );
     }
