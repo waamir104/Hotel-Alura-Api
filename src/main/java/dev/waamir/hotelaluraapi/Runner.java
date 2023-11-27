08fdb326ef94350423e17f618a5963d13283ffcf
waamir104 <waamirdev@gmail.com>
2023-11-26 22:38:45 -0400

    Optimizing imports

--- src/main/java/dev/waamir/hotelaluraapi/Runner.java
+++ src/main/java/dev/waamir/hotelaluraapi/Runner.java
@@ -1,6 +1,5 @@
 package dev.waamir.hotelaluraapi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
