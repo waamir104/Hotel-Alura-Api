855d0a3b366ac6feb4a35b150f9b5de75dcc992b
unknown <waamirdev@gmail.com>
2023-11-27 08:24:49 -0500

    Optimizing imports

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/config/ApplicationConfig.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/config/ApplicationConfig.java
@@ -6,8 +6,6 @@ import java.util.Base64.Encoder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
