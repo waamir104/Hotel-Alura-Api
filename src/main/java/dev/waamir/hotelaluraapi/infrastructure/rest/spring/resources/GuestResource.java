c9b858b1df4c0a4d0541e17064d9636b96f7b0ae
waamir104 <waamirdev@gmail.com>
2023-12-01 22:57:48 -0400

    Adding the Guest Resource with all the methods implemented

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/GuestResource.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/GuestResource.java
@@ -1,20 +1,33 @@
 package dev.waamir.hotelaluraapi.infrastructure.rest.spring.resources;
 
+import java.util.Objects;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import dev.waamir.hotelaluraapi.adapter.dto.resource.MessageResponse;
-import dev.waamir.hotelaluraapi.adapter.dto.resource.Guest.GuestResponse;
+import dev.waamir.hotelaluraapi.adapter.dto.resource.Guest.GuestDto;
+import dev.waamir.hotelaluraapi.application.enumeration.EmailType;
+import dev.waamir.hotelaluraapi.application.model.EmailDetails;
 import dev.waamir.hotelaluraapi.domain.model.Guest;
+import dev.waamir.hotelaluraapi.domain.port.IEmailService;
 import dev.waamir.hotelaluraapi.domain.port.IGuestRepository;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.DuplicateRecordException;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.GuestNotFoundException;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -24,12 +37,33 @@ public class GuestResource {
 
 	@Autowired
 	private IGuestRepository<Guest> guestRepository;
+	@Autowired
+	private IEmailService emailService;
 	
 	@PostMapping("/register")
-	public ResponseEntity<MessageResponse> register() {
-		// TODO implement the logic to register a guest
+	public ResponseEntity<MessageResponse> register(
+		@RequestBody GuestDto request
+	) {
+		System.out.println(guestRepository.countByEmail(request.email()));
+		if (guestRepository.countByEmail(request.email()) != 0) throw new DuplicateRecordException("Guest's email is already in use.");
+		Guest guest = Guest.builder()
+			.name(request.name())
+			.lastName(request.lastName())
+			.email(request.email())
+			.phoneNumber(request.phoneNumber())
+			.idNumber(request.idNumber())
+			.birthDate(request.birthDate())
+			.build();
+		guestRepository.create(guest);
+		EmailDetails emailDetails = EmailDetails.builder()
+            .recipient(guest.getEmail())
+            .subject("Welcome!!")
+            .msgBody(emailService.getRegistrationMessage(guest, getRegistrationUrl()))
+            .type(EmailType.REGISTRATION)
+            .build();
+        emailService.sendEmail(emailDetails);
 		return ResponseEntity
-			.status(0)
+			.status(HttpStatus.OK)
 			.body(
 				MessageResponse.builder()
 					.message("Guest registered successfully")	
@@ -37,9 +71,46 @@ public class GuestResource {
 			);
 	}
 
-	@PostMapping("/update")
-	public ResponseEntity<MessageResponse> update() {
-		// TODO implement the logic to update a guest
+	@PutMapping("/update")
+	public ResponseEntity<MessageResponse> update(
+		@RequestBody GuestDto request
+	) {
+		Guest guest;
+		Guest updatedGuest;
+		Optional<Guest> guestOp = guestRepository.getByEmail(request.email());
+		if (!guestOp.isPresent()){
+			guest = guestRepository.getByIdNumber(request.idNumber()).orElseThrow(
+				() -> {
+					throw new GuestNotFoundException("");
+				}
+			);
+		} else {
+			guest = guestOp.get();
+		}
+		if (Objects.isNull(guest.getIdNumber())) {
+			updatedGuest = Guest.builder()
+				.id(guest.getId())
+				.idNumber(request.idNumber())
+				.name(request.name())
+				.lastName(request.lastName())
+				.email(request.email())
+				.phoneNumber(request.phoneNumber())
+				.birthDate(request.birthDate())
+				.bookings(guest.getBookings())
+				.build();
+		} else {
+			updatedGuest = Guest.builder()
+				.id(guest.getId())
+				.idNumber(guest.getIdNumber())
+				.name(request.name())
+				.lastName(request.lastName())
+				.email(request.email())
+				.phoneNumber(request.phoneNumber())
+				.birthDate(request.birthDate())
+				.bookings(guest.getBookings())
+				.build();
+		}
+		guestRepository.update(updatedGuest);
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(
@@ -49,25 +120,47 @@ public class GuestResource {
 			);
 	}
 
-	@PostMapping("/list")
-	public ResponseEntity<Page<GuestResponse>> list(
+	@GetMapping("/list")
+	public ResponseEntity<Page<GuestDto>> list(
 		@PageableDefault(size = 10) Pageable pagination
 	) {
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(
-				guestRepository.list(pagination).map(GuestResponse::new)
+				guestRepository.list(pagination).map(GuestDto::new)
 			);
 	}
 
-	@PostMapping("/idNumber/{idNumber}")
-	public ResponseEntity<GuestResponse> fetchByIdNumber(@PathVariable @NotNull @Positive Long idNumber) {
-		Guest guest;
-		// TODO implement the logic to fetch guest by id number
+	@GetMapping("/idNumber/{idNumber}")
+	public ResponseEntity<GuestDto> fetchByIdNumber(@PathVariable @NotNull @Positive Long idNumber) {
+		Guest guest = guestRepository.getByIdNumber(idNumber).orElseThrow(
+			() -> {
+				throw new GuestNotFoundException("");
+			}
+		);
 		return ResponseEntity
-			.status(0)
+			.status(HttpStatus.OK)
 			.body(
-				new GuestResponse(guest)
+				new GuestDto(guest)
 			);
 	}
+
+	@GetMapping("/email/{email}")
+	public ResponseEntity<GuestDto> fetchByIdNumber(@PathVariable @NotNull @Email String email) {
+		Guest guest = guestRepository.getByEmail(email).orElseThrow(
+			() -> {
+				throw new GuestNotFoundException("");
+			}
+		);
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(
+				new GuestDto(guest)
+			);
+	}
+
+	private String getRegistrationUrl() {
+		// TODO change to the front url
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("api/v1/user/register/").toUriString();
+    }
 }
