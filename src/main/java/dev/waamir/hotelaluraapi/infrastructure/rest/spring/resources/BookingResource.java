9ef2dc9a7b702d5b8d8e8bb5e5f015d8bbfcecca
waamir104 <waamirdev@gmail.com>
2023-12-03 01:16:07 -0400

    Adding the implementation of the methods in the BookingResource controller

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/BookingResource.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/BookingResource.java
@@ -1,8 +1,12 @@
 package dev.waamir.hotelaluraapi.infrastructure.rest.spring.resources;
 
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,18 +20,37 @@ import dev.waamir.hotelaluraapi.adapter.dto.resource.MessageResponse;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.Booking.BookingRequest;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.Booking.BookingResponse;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.Booking.BookingUpdateRequest;
+import dev.waamir.hotelaluraapi.domain.model.Booking;
+import dev.waamir.hotelaluraapi.domain.model.Guest;
+import dev.waamir.hotelaluraapi.domain.model.Room;
+import dev.waamir.hotelaluraapi.domain.model.RoomType;
+import dev.waamir.hotelaluraapi.domain.port.IBookingRepository;
+import dev.waamir.hotelaluraapi.domain.port.IGuestRepository;
+import dev.waamir.hotelaluraapi.domain.port.IRoomRepository;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.ApiNotFoundException;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.GuestNotFoundException;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.RoomNotFoundException;
 
 @RestController
 @RequestMapping("/api/v1/booking")
 public class BookingResource {
-    
+
+    @Autowired
+    private IBookingRepository<Booking> bookingRepository;
+    @Autowired
+    private IGuestRepository<Guest> guestRepository;
+    @Autowired
+    private IRoomRepository<Room> roomRepository;
 
     @GetMapping("/list")
     public ResponseEntity<Page<BookingResponse>> list (
         @PageableDefault(size = 10) Pageable pagination
     ) {
-        // TODO implement the logic
-        return null;
+        return ResponseEntity
+            .status(200)
+            .body(
+                bookingRepository.list(pagination).map(BookingResponse::new)
+            );
     }
 
     @GetMapping("/list/{guestEmail}")
@@ -35,31 +58,86 @@ public class BookingResource {
         @PageableDefault(size = 10) Pageable pagination,
         @PathVariable String email
     ) {
-        // TODO implement the logic
-        return null;
+        Guest guest = guestRepository.getByEmail(email).orElseThrow(
+            () -> {
+                throw new GuestNotFoundException("");
+            }
+        );
+        return ResponseEntity
+            .status(200)
+            .body(
+                bookingRepository.listByGuestEmail(guest.getEmail(), pagination).map(BookingResponse::new)
+            );
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BookingResponse> fetchById (
         @PathVariable long id
     ) {
-        // TODO implement the logic
-        return null;
+        Booking booking = bookingRepository.getById(id).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("");
+            }
+        );
+        return ResponseEntity
+            .status(200)
+            .body(
+                new BookingResponse(booking)
+            );
     }
 
     @PostMapping("/register")
     public ResponseEntity<MessageResponse> register (
         @RequestBody BookingRequest requestDto
     ) {
-        // TODO implement the logic
-        return null;
+        Guest guest = guestRepository.getByEmail(requestDto.guestEmail()).orElseThrow(
+            () -> {
+                throw new GuestNotFoundException("");
+            }
+        );
+        Room room = roomRepository.getByNumber(requestDto.roomNumber()).orElseThrow(
+            () -> {
+                throw new RoomNotFoundException("");
+            }
+        );
+        RoomType roomType = room.getRoomType();
+        long days = ChronoUnit.DAYS.between(requestDto.checkIn(), requestDto.checkOut());
+        double totalPrice = roomType.getDailyPrice() * days;
+        Booking newBooking = Booking.builder()
+            .checkIn(requestDto.checkIn())
+            .checkOut(requestDto.checkOut())
+            .guest(guest)
+            .room(room)
+            .totalPrice(totalPrice)
+            .build();
+        newBooking = bookingRepository.create(newBooking);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(
+                MessageResponse.builder()
+                    .message(String.format("Booking created with id: %d", newBooking.getId()))
+                    .build()
+            );
     }
 
     @PutMapping("/update")
     public ResponseEntity<MessageResponse> update (
         @RequestBody BookingUpdateRequest requestDto
     ) {
-        // TODO implement the logic
-        return null;
+        Booking booking = bookingRepository.getById(requestDto.id()).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("");
+            }
+        );
+        booking.setCheckIn(requestDto.checkIn());
+        booking.setCheckOut(requestDto.checkOut());
+        bookingRepository.update(booking);
+        return ResponseEntity
+            .status(200)
+            .body(
+                MessageResponse.builder()
+                    .message("Booking updated succesfully")
+                    .build()
+            );
     }
 }
