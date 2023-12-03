fb7e62762f2446d74eec2170c4ce0ece1120cbce
waamir104 <waamirdev@gmail.com>
2023-12-03 17:47:59 -0400

    Adding the implementation of the methods

--- src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/BookingPaymentTypeResource.java
+++ src/main/java/dev/waamir/hotelaluraapi/infrastructure/rest/spring/resources/BookingPaymentTypeResource.java
@@ -1,6 +1,9 @@
 package dev.waamir.hotelaluraapi.infrastructure.rest.spring.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +18,14 @@ import dev.waamir.hotelaluraapi.adapter.dto.resource.MessageResponse;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.BookingPaymentType.BookingPaymentTypeRegisterRequest;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.BookingPaymentType.BookingPaymentTypeResponse;
 import dev.waamir.hotelaluraapi.adapter.dto.resource.BookingPaymentType.BookingPaymentTypeUpdateRequest;
+import dev.waamir.hotelaluraapi.domain.model.Booking;
 import dev.waamir.hotelaluraapi.domain.model.BookingPaymentType;
+import dev.waamir.hotelaluraapi.domain.model.PaymentType;
 import dev.waamir.hotelaluraapi.domain.port.IBookingPaymentTypeRepository;
+import dev.waamir.hotelaluraapi.domain.port.IBookingRepository;
+import dev.waamir.hotelaluraapi.domain.port.IPaymentTypeRepository;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.ApiNotFoundException;
+import dev.waamir.hotelaluraapi.infrastructure.rest.spring.exception.GenericException;
 import jakarta.validation.constraints.NotNull;
 
 @RestController
@@ -25,36 +34,109 @@ public class BookingPaymentTypeResource {
     
     @Autowired
     private IBookingPaymentTypeRepository<BookingPaymentType> bookingPaymentTypeRepository;
+    @Autowired
+    private IBookingRepository<Booking> bookingRepository;
+    @Autowired
+    private IPaymentTypeRepository<PaymentType> paymentTypeRepository;
 
     @GetMapping("/booking/{id}")
-    public ResponseEntity<BookingPaymentTypeResponse> fetchByBooking (
+    public ResponseEntity<List<BookingPaymentTypeResponse>> fetchByBooking (
         @PathVariable Long bookingId
     ){
-        // TODO implement the logic
-        return null;
+        Booking booking = bookingRepository.getById(bookingId).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Booking not found.");
+            }
+        );
+        List<BookingPaymentType> list = bookingPaymentTypeRepository.listByBooking(booking);
+        return ResponseEntity
+            .status(200)
+            .body(
+                list.stream().map(BookingPaymentTypeResponse::new).toList()
+            );
     }
 
     @PostMapping("/register")
     public ResponseEntity<MessageResponse> register(
         @RequestBody BookingPaymentTypeRegisterRequest request
     ) {
-        // TODO implement the logic
-        return null;
+        Booking booking = bookingRepository.getById(request.bookingId()).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Booking not found.");
+            }
+        );
+        PaymentType paymentType = paymentTypeRepository.getById(request.paymentTypeId()).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Payment type not found.");
+            }
+        );
+        double totalPaymentPercentage = booking.getBookingPaymentTypes().stream()
+            .mapToDouble(BookingPaymentType::getPaymentPercentage).sum();
+        if (request.paymentPercentage() > (100 - totalPaymentPercentage)) throw new GenericException("The payment percentage exceeds the limit.", HttpStatus.BAD_REQUEST);
+        BookingPaymentType newBookingPaymentType = BookingPaymentType.builder()
+            .paymentPercentage(request.paymentPercentage())
+            .booking(booking)
+            .paymentType(paymentType)
+            .build();
+        newBookingPaymentType = bookingPaymentTypeRepository.create(newBookingPaymentType);
+        return ResponseEntity
+            .status(201)
+            .body(
+                MessageResponse.builder()
+                    .message(String.format("Booking Payment Type registered with id: ", newBookingPaymentType.getId()))
+                    .build()
+            );
     }
 
     @PutMapping("/update")
     public ResponseEntity<MessageResponse> update(
         @RequestBody BookingPaymentTypeUpdateRequest request
     ) {
-        // TODO implement the logic
-        return null;
+        BookingPaymentType bookingPaymentTypeToUpdate = bookingPaymentTypeRepository.getById(request.id()).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Booking Payment Type not found.");
+            }
+        );
+        PaymentType paymentType = paymentTypeRepository.getById(request.paymentTypeId()).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Payment Type not found.");
+            }
+        );
+        Booking booking = bookingPaymentTypeToUpdate.getBooking();
+        double totalPaymentPercentage = booking.getBookingPaymentTypes().stream()
+            .filter(bookingPaymentType -> !(bookingPaymentType.getId() == request.id()))
+            .mapToDouble(BookingPaymentType::getPaymentPercentage)
+            .sum();
+        totalPaymentPercentage += request.paymentPercentage();
+        if (totalPaymentPercentage > 100) throw new GenericException("The payment percentage exceeds the limit.", HttpStatus.BAD_REQUEST);
+        bookingPaymentTypeToUpdate.setPaymentPercentage(request.paymentPercentage());
+        bookingPaymentTypeToUpdate.setPaymentType(paymentType);
+        bookingPaymentTypeRepository.update(bookingPaymentTypeToUpdate);
+        return ResponseEntity
+            .status(200)
+            .body(
+                MessageResponse.builder()
+                    .message("Booking Payment Type updated successfully")
+                    .build()
+            );
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<MessageResponse> delete (
         @PathVariable @NotNull Long id
     ) {
-        // TODO implement the logic
-        return null;
+        BookingPaymentType bookingPaymentType = bookingPaymentTypeRepository.getById(id).orElseThrow(
+            () -> {
+                throw new ApiNotFoundException("Booking Payment Type not found.");
+            }
+        );
+        bookingPaymentTypeRepository.delete(bookingPaymentType);
+        return ResponseEntity
+            .status(200)
+            .body(
+                MessageResponse.builder()
+                    .message("Booking Payment Type deleted successfully")
+                    .build()
+            );
     }
 }
