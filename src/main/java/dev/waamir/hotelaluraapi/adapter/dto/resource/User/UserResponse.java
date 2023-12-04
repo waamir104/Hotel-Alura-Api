7636d806a5d4a236376993142a39da59256cd564
waamir104 <waamirdev@gmail.com>
2023-12-03 20:21:39 -0400

    Changing the structure of the UserResponse file and implementing it within the UserResourceService

--- src/main/java/dev/waamir/hotelaluraapi/adapter/dto/resource/User/UserResponse.java
+++ src/main/java/dev/waamir/hotelaluraapi/adapter/dto/resource/User/UserResponse.java
@@ -11,4 +11,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserResponse {
     private String token;
+    private String role;
 }
--- src/main/java/dev/waamir/hotelaluraapi/application/service/Resource/UserResourceService.java
+++ src/main/java/dev/waamir/hotelaluraapi/application/service/Resource/UserResourceService.java
@@ -42,6 +42,7 @@ public class UserResourceService {
         String jwt = jwtService.generateJwt(user);
         return UserResponse.builder()
             .token(jwt)
+            .role(user.getRole().getName())
             .build();
     }
 
@@ -58,6 +59,7 @@ public class UserResourceService {
         String jwt = jwtService.generateJwt(user);
         return UserResponse.builder()
             .token(jwt)
+            .role(user.getRole().getName())
             .build();
     }
     
